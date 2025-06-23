758cee876fe457f7e997b27c4622271742ccbc8f
Vladislav Tsirulev <Vladislav.Tsirulev@columbusglobal.com>
2025-06-23 11:36:47 +0300

    fix ignore

---
+++ MiniJira.Infrastructure/Logs/LogRepository.cs
@@ -0,0 +1,55 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
+using MiniJira.Domain.Entities;
+using MiniJira.Repository;
+
+namespace MiniJira.Infrastructure.Logs
+{
+    public class LogRepository : ILogRepository
+    {
+        public async Task CreateLog(long taskId, int userId, string description, CancellationToken cancellationToken)
+        {
+            var command = new CommandDefinition(
+                """
+                INSERT INTO tasks_logs (
+                                 task_id, 
+                                 changed_task_user_id, 
+                                 description
+                                 ) values
+                (@taskId, @changedTaskUserId, @description)
+                """,
+                new 
+                {
+                    taskId = taskId,
+                    changedTaskUserId = userId,
+                    description = description
+                },
+                cancellationToken: cancellationToken);
+            using var connection = await DatabaseConnectionHelper.GetOpenConnection(cancellationToken);
+            await connection.ExecuteAsync(command);
+        }
+
+        public async Task<LogEntity[]> GetLogsByTaskId(int taskId, CancellationToken cancellationToken)
+        {
+            var command = new CommandDefinition(
+                """
+                select 
+                    task_id as TaskId,
+                    description as Description,
+                    changed_task_user_id as UserNameWhoChanged,
+                    created_at as ActionDate
+                from tasks_logs
+                where task_id = @taskId;
+                """,
+                new 
+                {
+                    taskId = taskId,
+                },
+                cancellationToken: cancellationToken);
+            using var connection = await DatabaseConnectionHelper.GetOpenConnection(cancellationToken);
+            return (await connection.QueryAsync<LogEntity>(command)).ToArray();
+        }
+    }
+}
